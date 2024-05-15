b9e180af292fe2101856fc10ed5705894d9a7640
wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-05-15 15:30:59 +0530

    created verify screen

--- lib/screens/verify/verify_product_screen.dart
+++ lib/screens/verify/verify_product_screen.dart
@@ -1,5 +1,8 @@
+import 'package:compass/utils/constants.dart';
 import 'package:compass/widgets/loading_dialog.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class VerifyProductScreen extends StatelessWidget {
   const VerifyProductScreen({super.key});
@@ -8,27 +11,100 @@ class VerifyProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("hello"),
-          // bottom: ,
-          flexibleSpace: Text("hell"),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            label: Text("SCAN BARCODE"),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          // appBar: AppBar(
+          //   title: Text("hello"),
+          //   // bottom: ,
+          //   flexibleSpace: Text("hell"),
+          //
+          // ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Image(
+                        image: const AssetImage("assets/logo.png"),
+                        width: 110,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ShaderMask(
+                        blendMode: BlendMode.srcIn,
+                        shaderCallback: (Rect bounds) {
+                          return LinearGradient(
+                            colors: [
+                              Colors.red,
+                              Theme.of(context).primaryColor
+                            ],
+                          ).createShader(bounds);
+                        },
+                        child: Text(
+                          "${appName} Attest ".toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            // color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Authenticate Product BarCodes",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text(
+                        style:
+                            TextStyle(color: Theme.of(context).disabledColor),
+                        '"Verify product authenticity with our barcode check. Ensure your items are genuine and trustworthy."',
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/verify.svg",
+                        width: 150,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 50,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
 
-        ),
-        body:LinearProgressIndicator()
-
-        // Expanded(
-        //   child: Center(
-        //
-        //     child: MaterialButton(
-        //       onPressed: () {
-        //         print("helllo");
-        //         compassLoading();
-        //       },
-        //       child: Text("Search"),
-        //     ),
-        //   ),
-        // ),
-      ),
+          // Expanded(
+          //   child: Center(
+          //
+          //     child: MaterialButton(
+          //       onPressed: () {search
+          //         print("helllo");
+          //         compassLoading();
+          //       },
+          //       child: Text("Search"),
+          //     ),
+          //   ),
+          // ),
+          ),
     );
   }
 }
